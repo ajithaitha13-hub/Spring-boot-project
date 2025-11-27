d29f6e8dbfcc2589a2f678720329fba6e45d805c
ajithaitha13-hub <ajithaitha13@gmail.com>
2025-11-26 20:39:13 -0500

    Add second Spring Boot project

---
+++ project 2/src/main/java/com/example/simpleWebApp/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.example.simpleWebApp.service;
+
+import com.example.simpleWebApp.model.Product;
+import com.example.simpleWebApp.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepo repo;
+
+
+//    List<Product> products = new ArrayList<>(Arrays.asList(
+//            new Product(101, "Iphone", 50000),
+//            new Product(102, "Asus Laptop", 60000),
+//            new Product(103, "Apple Watch", 75000)));
+
+    public List<Product> getProducts(){
+
+        return repo.findAll();
+    }
+
+    public Product getProductById(int prodId){
+        return repo.findById(prodId).orElse(new Product());
+    }
+
+    public void addProduct(Product prod){
+        repo.save(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        repo.save(prod);
+    }
+
+    public void deleteProduct(int prodId) {
+        repo.deleteById(prodId);
+    }
+}
